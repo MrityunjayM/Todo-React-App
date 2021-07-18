aef5e1cf5135df8186f0021125a1583d7884517c
Mrityunjay Mishra <you@example.com>
2021-07-18 23:36:26 +0530

    change in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="card p-0 text-light text-center bg-transparent rounded-0">
                 <div className="card-footer px-0 py-3 d-flex justify-content-between">
                     <p className="ps-3 mb-1">&copy;2021 </p>
-                    <a className="nav nav-link text-white p-0" href='/#About'>About Us!</a>
+                    <a className="nav nav-link text-white p-0 me-4" href='/#About'>About Us!</a>
                     {/* <p className="me-3 mb-1 text-muted">By : Mrityunjay Mishra</p> */}
                 </div>
             </div>  
